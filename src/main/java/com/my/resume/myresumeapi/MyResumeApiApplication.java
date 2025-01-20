ef583dc7bf27776aa33d3bc8e1e663c824512bdf
Jagruti Hirpara <jagrutilakhani1989@gmail.com>
2025-01-20 23:00:32 +0100

    Add files via upload

---
+++ src/main/java/com/my/resume/myresumeapi/MyResumeApiApplication.java
@@ -0,0 +1,13 @@
+package com.my.resume.myresumeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResumeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyResumeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/resume/myresumeapi/MyResumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.resume.myresumeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
